b0f935dd0762e011341cde62f429999957f8882c
Q2P <97294453+RootQ2P@users.noreply.github.com>
2023-03-11 20:27:37 +0000

    Add Qb inv support

--- e-cokeruns/server.lua
+++ e-cokeruns/server.lua
@@ -1,9 +1,11 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local ox_inventory = exports.ox_inventory
 
-
-
-
 RegisterNetEvent('e-cokeruns:server:addMeth',function (plate)
-    exports.ox_inventory:AddItem("trunk"..plate, Config.itemName, Config.itemAmount)
+    if Config.qbinv then
+    TriggerEvent("inventory:server:addTrunkItems", plate, Config.qbitemName)
+else
+        exports.ox_inventory:AddItem("trunk"..plate, Config.itemName, Config.itemAmount)  
+        print("debug") 
+    end
 end)
