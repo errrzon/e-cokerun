4cb717c05d5f4dc8f714014c8e426dc5741b63c0
errrzon <106036010+errrzon@users.noreply.github.com>
2023-03-12 00:20:25 +0100

    Update server.lua
    added support for qb-inventory

--- e-cokeruns/server.lua
+++ e-cokeruns/server.lua
@@ -1,9 +1,10 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local ox_inventory = exports.ox_inventory
 
-
-
-
 RegisterNetEvent('e-cokeruns:server:addMeth',function (plate)
-    exports.ox_inventory:AddItem("trunk"..plate, Config.itemName, Config.itemAmount)
+    if Config.qbInventory then
+        TriggerEvent("inventory:server:addTrunkItems", plate, Config.qbItemName)
+    else
+        exports.ox_inventory:AddItem("trunk"..plate, Config.itemName, Config.itemAmount)
+    end
 end)
